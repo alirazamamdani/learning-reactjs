7ba45a58b9134e60e63c856664ff7fdeadb9e2eb
alimamdani <Ali.mamdani@hotmail.com>
2021-09-02 23:27:45 +0500

    lesson 7

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log("Click me")
+    }
+    const handleClickAgain = (name, e) => {
+        console.log("Hello "+ name, e.target)
+    }    
+
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick} >Click Me</button>
+            <button onClick={(e)=> handleClickAgain("Ali Raza", e)}>Click me Again</button>
         </div>
      );
 }
